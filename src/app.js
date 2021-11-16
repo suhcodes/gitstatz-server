441891d17c6b0912985f30e2d197879f503e5d80
Suély Moraes <suely.antunes@mundipagg.com>
2021-11-16 00:40:08 -0300

    🎉 init project

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
 import axios from "axios";
 import pkg from "lodash";
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
+const env = dotenv.config().parsed;
 const app = express();
 const { get } = pkg;
 
@@ -12,47 +14,37 @@ const { get } = pkg;
   secret client ID
   and code
 */
-const clientID = process.env.SERVER_APP_CLIENT_ID;
-const secretClientID = process.env.SERVER_APP_CLIENT_SECRET_ID;
-const githubAuth = process.env.SERVER_APP_GITHUB_AUTH_URI;
+const clientID = env.SERVER_APP_CLIENT_ID;
+const secretClientID = env.SERVER_APP_CLIENT_SECRET_ID;
+const githubAuth = env.SERVER_APP_GITHUB_AUTH_URI;
 
 const COOKIE_NAME = "github-jwt";
 
-const getGithubAuthToken = ({ code }) => {
+const getGithubAuthToken = async ({ code }) => {
   const requestURL = `${githubAuth}/access_token?client_id=${clientID}&client_secret=${secretClientID}&code=${code}`;
-  return axios
+  const githubToken = await axios
     .post(requestURL)
-    .then((res) => {
-      const decoded = new URLSearchParams(res.data);
-      const token = decoded.get("access_token");
-      return token;
-    })
+    .then((res) => res.data)
     .catch((err) => {
       throw err;
     });
+  const decoded = new URLSearchParams(githubToken);
+  const token = decoded.get("access_token");
+  return token;
 };
 
-app.get("/api/auth/github", (req, res) => {
+app.get("/api/auth/github", async (req, res) => {
   const code = get(req, "query.code");
   const path = get(req, "query.path", "/");
   if (!code) {
     throw new Error({ message: "No code :(", code: 400 });
   }
-  return getGithubAuthToken({ code })
-    .then((token) => {
-      res.cookie(COOKIE_NAME, token, {
-        httpOnly: true,
-        domain: "localhost",
-      });
-      res.redirect(`${path}`);
-    })
-    .catch((err) => {
-      throw err;
-    });
-});
-
-app.get("/", (req, res) => {
-  res.send("Olá! Essa é a API do gitstatz :)");
+  const token = await getGithubAuthToken({ code });
+  res.cookie(COOKIE_NAME, token, {
+    httpOnly: true,
+    domain: "localhost",
+  });
+  res.redirect(`${path}`);
 });
 
 app.listen(port, () => {});
