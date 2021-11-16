73965cf5f1280aad35c435678efe0720a0efe67e
Suély Moraes <suely.antunes@mundipagg.com>
2021-11-15 22:14:47 -0300

    🎉 init project

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# gitstatz-server
-Server express app to handle GitHub OAuth conversion from code to jwt
+<h1>
+  <div align="center">
+    <img src="./logos/Gitstatz.png" />
+  </div>
+</h1>
+
+**This is a simple backend solution to help convert the code received from github's OAuth into a JWT.**
+
+---
+##### :wrench: Configuration:
+- After creating your own Github OAuth application, you'll need to set a few env variables. This can be done through creating a `.env` file with the following information:
+```sh
+SERVER_APP_CLIENT_ID="YOUR CLIENT ID"
+SERVER_APP_CLIENT_SECRET_ID="YOUR CLIENT SECRET ID"
+SERVER_APP_GITHUB_AUTH_URI="https://github.com/login/oauth"
+SERVER_APP_GITHUB_API_URI="https://api.github.com"
+SERVER_APP_PUBLIC_PATH="/"
+SERVER_APP_CLIENT_REDIRECT_URL="http://localhost:3000/"
+```
+> There's an exemple file with all the variables named `.env.exemple`.
+
+- Make sure that your Github OAuth callback URL is the same as the one you'll use for your API.
+
+```
+[YOUR_ROOT_URL]/api/auth/github
+```
+
+> :books: More about how to use github's api [here](https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps)
---
+++ src/app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+import pkg from "lodash";
+
+const env = dotenv.config().parsed;
+const app = express();
+const { get } = pkg;
+
+/* 
+  Set up github URL
+  with client ID,
+  secret client ID
+  and code
+*/
+const clientID = env.SERVER_APP_CLIENT_ID;
+const secretClientID = env.SERVER_APP_CLIENT_SECRET_ID;
+const githubAuth = env.SERVER_APP_GITHUB_AUTH_URI;
+const redirectURL = env.SERVER_APP_CLIENT_REDIRECT_URL;
+
+const COOKIE_NAME = "github-jwt";
+
+const getGithubAuthToken = async ({ code }) => {
+  const requestURL = `${githubAuth}/access_token?client_id=${clientID}&client_secret=${secretClientID}&code=${code}`;
+  const githubToken = await axios
+    .post(requestURL)
+    .then((res) => res.data)
+    .catch((err) => {
+      throw err;
+    });
+  const decoded = new URLSearchParams(githubToken);
+  const token = decoded.get("access_token");
+  return token;
+};
+
+app.get("/api/auth/github", async (req, res) => {
+  const code = get(req, "query.code");
+  const path = get(req, "query.path", "/");
+  if (!code) {
+    throw new Error({ message: "No code :(", code: 400 });
+  }
+  const token = await getGithubAuthToken({ code });
+  res.cookie(COOKIE_NAME, token, {
+    httpOnly: true,
+    domain: "localhost",
+  });
+  res.redirect(`${redirectURL}${path}`);
+});
+
+app.listen(4000, () => {});
